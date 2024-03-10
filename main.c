3e6685b5a884d5b50ec32420990ea89d1cab2604
Nizarll <forsubsofmy@gmail.com>
2024-03-10 18:44:29 +0000

    reworked the code structure

--- main.c
+++ main.c
@@ -2,22 +2,15 @@
 #include <stdio.h>
 
 #include "libs/renderer.h"
+#include <ctype.h>
 #include <raylib.h>
 #include <raymath.h>
+#include <signal.h>
+
+static const int screenWidth = 800;
+static const int screenHeight = 450;
 
 int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
   renderer_init(screenWidth, screenHeight, "cool pool window");
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    printf("YOUR FPS IS : %i \n", GetFPS());
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-    EndDrawing();
-  }
-  CloseWindow(); // Close window and OpenGL context
   return 0;
 }
--- src/renderer.c
+++ src/renderer.c
@@ -1,226 +1,169 @@
 #include "../libs/renderer.h"
+#include <raylib.h>
+#include <stdint.h>
 
 #define FPS_CAP (4999)
 #define PLAYER_HEIGHT (12)
 #define PLAYER_WIDTH (6)
 
-typedef enum {
-  PLAYER,
-  COLLIDEABLE_OBJ,
-  PARTICLE,
-} Kind;
+#define OPT(T) (option(T))
 
-typedef enum {
-  IDLING,
-  WALKING,
-  RUNNING,
-  ATTACKING,
-  JUMPING,
-  ATTACKED,
-} State;
-
-char *get_state_name(State state) {
-  switch (state) {
-  case (IDLING):
-    return "IDLING";
-  case (RUNNING):
-    return "RUNNING";
-  case (WALKING):
-    return "WALKING";
-  case (ATTACKING):
-    return "ATTACKING";
-  case (JUMPING):
-    return "JUMPING";
-  case (ATTACKED):
-    return "ATTACKED";
-  default:
-    return "BRUH WTF IS THIS STATE FARK OFF";
-  }
-  return NULL;
+float option(float t) {
+  if (!t)
+    return 1;
+  return t;
 }
 
 typedef struct {
-  uint16_t frame_count;
-  uint16_t frame_index;
-  uint16_t current_frame;
-  char *name;
-  char *path_to_anim;
+  uint16_t keys_pressed;
+} InputTracker;
+
+typedef struct {
   Rectangle frame;
+  char *path;
+  Texture2D sprite_sheet;
+  uint16_t length;
+  uint16_t frames_counter;
+  uint16_t current_frame;
 } Animation;
 
 typedef struct {
-  char *name;
-  State state;
-  bool state_ready;
-} StateSelector;
+  Animation *const animations;
+  uint16_t current_anim_index;
+  uint16_t length;
+} AnimationGroup;
 
-struct Object {
-  const Animation *animations; // read only
-  const StateSelector *states; // read only
-  Animation animation;
-  StateSelector state;
-  StateSelector default_state;
-  uint16_t animations_len;
-  Kind kind;
+typedef struct {
   Vector2 position;
-  Vector2 hitbox;
-  uint16_t rotation;
-  Color color;
-  Texture2D texture;
-};
+  Vector2 size;
+  AnimationGroup anim_group;
+  InputTracker *input_tracker;
+} Entity;
 
-static Animation set_anim(Object *object, const char *name) {
-  for (int i = 0; i < object->animations_len; i++) {
-    if (strcmp(object->animations[i].name, name) == 0)
-      return object->animations[i];
+static const Animation idle_anim = (Animation){
+    .path = "./resources/character.png",
+    .length = 1,
+};
+static const Animation walk_anim = (Animation){
+    .path = "./resources/character_walk_cycle.png",
+    .length = 4,
+};
+static const Animation run_anim = {0};
+static const Animation hit_anim = {0};
+
+static Animation player_animations[] = {idle_anim, run_anim, walk_anim,
+                                        hit_anim};
+
+static Entity player = (Entity){
+    .size = (Vector2){.x = 5, .y = 5},
+    .position = (Vector2){.x = 0, .y = 0},
+    .anim_group = (AnimationGroup){
+        .animations = player_animations,
+        .length = sizeof(player_animations) / sizeof(player_animations[0]),
+        .current_anim_index = 0,
+    }};
+
+void handle_input(Entity *entity) {
+  if (entity->input_tracker == NULL)
+    return;
+  InputTracker *input_tracker = entity->input_tracker;
+  if (IsKeyDown(KEY_A)) {
+    input_tracker->keys_pressed += 1;
   }
-  return (Animation){};
+  if (IsKeyDown(KEY_D)) {
+    input_tracker->keys_pressed += 1;
+  }
 }
 
-static void handle_anims(Object *object) {
-  if (object->kind == PLAYER) {
-    if (strcmp(object->state.name, "Walking") == 0) {
-      object->animation.frame_index = 0;
-      object->animation.current_frame++;
-      if (object->animation.current_frame > object->animation.frame_count - 1)
-        object->animation.current_frame = 0;
+//  Rectangle frame;
+//  char *path;
+//  Texture2D sprite_sheet;
+//  uint16_t length;
+//  uint16_t frames_counter;
+//  uint16_t current_frame;
+//} Animation;
 
-      object->animation.frame.x = (float)object->animation.current_frame *
-                                  (float)object->texture.width /
-                                  object->animation.frame_count;
-    } else if (strcmp(object->state.name, "Idling") == 0) {
-      // play pause animation
-    }
-  } else if (object->kind == COLLIDEABLE_OBJ) {
-
-  } else {
-  }
-}
-
-static const StateSelector player_states[] = {
-    {.name = "Walking", .state = WALKING, .state_ready = false},
-    //{.name = "Walking", .state = WALKING, .state_ready = false},
-    //{.name = "Walking", .state = WALKING, .state_ready = false},
-};
-
-static const StateSelector enemy_states[] = {
-    {.name = "Walking", .state = WALKING, .state_ready = false},
-    //{.name = "Attacking", .state = ATTACKING, .state_ready = false},
-    //{.name = "Dashing", .state = DASHING, .state_ready = false},
-};
-
-/* typedef struct {
-* uint16_t frame_count;
-*  uint16_t frame_index;
-*  uint16_t current_frame;
-*  char *name;
-*  char *path_to_anim;
-*  Rectangle frame;
-} Animation; */
-
-static Animation *anims_init(Kind kind) {
-  Animation *animations;
-  switch (kind) {
-  case PLAYER: {
-    int i = 0;
-    animations = malloc(sizeof(player_states) / sizeof(player_states[0]));
-    animations[0] = (Animation){
-        .frame_count = 4,
-        .frame_index = 0,
-        .current_frame = 0,
-        .name = "Walking",
-        .path_to_anim = "./resources/character_run_cycle.png",
-        .frame =
-            (Rectangle){
-
-            },
+void init_entity(Entity *entity) {
+  if (entity->anim_group.length <= 0)
+    return;
+  for (int i = 0; i < entity->anim_group.length; i++) {
+    if (!(entity->anim_group.animations[i].path))
+      continue;
+    entity->anim_group.animations[i].frames_counter = 0;
+    entity->anim_group.animations[i].current_frame = 0;
+    entity->anim_group.animations[i].sprite_sheet =
+        LoadTexture(entity->anim_group.animations[i].path);
+    entity->anim_group.animations[i].frame = (Rectangle){
+        .x = .0f,
+        .y = .0f,
+        .width = (float)entity->anim_group.animations[i].sprite_sheet.width /
+                 entity->anim_group.animations[i].length * OPT(entity->size.x),
+        .height = (float)entity->anim_group.animations[i].sprite_sheet.height /
+                  entity->anim_group.animations[i].length * OPT(entity->size.y),
     };
   }
-    return animations;
-  case PARTICLE:
-
-    return;
-  case COLLIDEABLE_OBJ:
-    return;
+}
+void destroy_entity(Entity *entity) {
+  free(entity->input_tracker);
+  free(entity->anim_group.animations);
+  for (int i = 0; i < entity->anim_group.length; i++) {
+    if (entity->anim_group.animations[i].path)
+      free((void *)entity->anim_group.animations[i].path);
   }
-  return NULL;
 }
 
-static void destroy_obj(Object *obj) { free(obj->animations); }
-
-static Object obj_init(Kind kind, Vector2 position, uint16_t size) {
-  Object obj = {0};
-  switch (kind) {
-  case PLAYER:
-    return obj = (Object){
-               .hitbox = (Vector2){.x = PLAYER_WIDTH, .y = PLAYER_HEIGHT},
-               .position =
-                   (Vector2){
-                       .x = position.x / 2.0,
-                       .y = position.y / 2.0,
-                   },
-               .animations = anims_init(kind),
-               .states = player_states,
-           };
-  case PARTICLE:
-    return obj = (Object){
-               .hitbox = (Vector2){.x = PLAYER_WIDTH, .y = PLAYER_HEIGHT},
-               .position =
-                   (Vector2){
-                       .x = position.x / 2.0,
-                       .y = position.y / 2.0,
-                   },
-               //    .texture_path = "./resources/character_run_cycle.png",
-               //    .texture = LoadTexture(obj.texture_path),
-               //    .texture.width = obj.texture.width * size,
-               //    .texture.height = obj.texture.height * size,
-               //    .states = player_states,
-           };
-  case COLLIDEABLE_OBJ:
-    return obj = (Object){
-               .hitbox = (Vector2){.x = PLAYER_WIDTH, .y = PLAYER_HEIGHT},
-               .position =
-                   (Vector2){
-                       .x = position.x / 2.0,
-                       .y = position.y / 2.0,
-                   },
-               //    .texture_path = "./resources/character_run_cycle.png",
-               //    .texture = LoadTexture(obj.texture_path),
-               //    .states = player_states,
-           };
+void handle_anims(Entity *entity) {
+  Animation curr_anim =
+      entity->anim_group.animations[entity->anim_group.current_anim_index];
+  curr_anim.frames_counter++;
+  if (curr_anim.frames_counter >= (60 / .2)) { // change later
+    curr_anim.frames_counter = 0;
+    curr_anim.current_frame++;
+    if (curr_anim.current_frame > curr_anim.length - 1)
+      curr_anim.current_frame = 0;
+    curr_anim.frame.x = (float)curr_anim.current_frame *
+                        (float)curr_anim.sprite_sheet.width / curr_anim.length;
   }
-  return obj;
+}
+void handle_movement(Entity *entity) {
+  if (IsKeyDown(KEY_A)) {
+    entity->position = Vector2Add(
+        entity->position,
+        (Vector2){.x = -(float)1 / entity->input_tracker->keys_pressed,
+                  .y = 0});
+  }
+  if (IsKeyDown(KEY_D)) {
+    entity->position = Vector2Add(
+        entity->position,
+        (Vector2){.x = (float)1 / entity->input_tracker->keys_pressed, .y = 0});
+  }
 }
 
 void renderer_init(const int width, const int height, const char *title) {
   InitWindow(width, height, title);
   SetTargetFPS(FPS_CAP);
-  Object player = obj_init(
-      PLAYER, (Vector2){.x = (float)width / 2, .y = (float)height / 2}, 5);
-  Rectangle frame_rec = {0.0f, 0.0f, (float)player.texture.width / 4,
-                         (float)player.texture.height};
-  int frames_counter = 0, current_frame = 0;
-  int frames_speed = 1;
+  init_entity(&player);
+
+  player.anim_group.current_anim_index = 1;
+
   while (!WindowShouldClose()) {
-    frames_counter++;
-    if (frames_counter >= (int)(60 / .15)) {
-      frames_counter = 0;
-      current_frame++;
-
-      if (current_frame > 3)
-        current_frame = 0;
-
-      frame_rec.x = (float)current_frame * (float)player.texture.width / 4;
-    }
+    handle_input(&player);
+    handle_movement(&player);
     BeginDrawing();
     ClearBackground(LIGHTGRAY);
-    DrawTextureRec(player.texture, frame_rec,
-                   (Vector2){
-                       .x = player.texture.width / 2.0,
-                       .y = player.texture.height / 2.0,
-                   },
-                   WHITE);
+    handle_anims(&player);
+    DrawTextureRec(
+        player.anim_group.animations[player.anim_group.current_anim_index]
+            .sprite_sheet,
+        player.anim_group.animations[player.anim_group.current_anim_index]
+            .frame,
+        (Vector2){
+            .x = 300.0f,
+            .y = 250.0f,
+        },
+        WHITE);
     EndDrawing();
   }
   CloseWindow();
+  destroy_entity(&player);
 }
