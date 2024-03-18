eb509d5e4c5ba652f8f411b51fd43df0c44209a9
Nizarll <forsubsofmy@gmail.com>
2024-03-18 18:24:30 +0000

    added state machine to better handle player states

--- libs/renderer.h
+++ libs/renderer.h
@@ -1,53 +1,7 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
-#include <errno.h>
-#include <pthread.h>
-#include <raylib.h>
-#include <raymath.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-
-typedef enum {
-  S_WALK,
-  S_IDLE,
-  S_JUMP,
-  S_M1,
-  S_BLOCK,
-  S_DASH,
-} StateKind;
-
-typedef struct {
-  StateKind kind;
-  void *extra_data;
-} State;
-
-typedef struct Animation {
-  const char *path;
-  Texture2D texture;
-  Rectangle frame;
-  // animation variables;
-  size_t length;
-  size_t counter;
-  size_t speed;
-  size_t curr_frame;
-} Animation;
-
-typedef struct Player {
-  Color color;
-  uint8_t rotation;
-  Vector2 position;
-  Vector2 size;
-  State state;
-  //
-  Animation *animations;
-  size_t animations_length;
-  size_t curr_anim_index;
-} Player;
+#include "./state.h"
 
 Player *player_init(Vector2 size, Color color);
 void renderer_init(const int width, const int height, const char *title);
---
+++ libs/state.h
@@ -0,0 +1,59 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <stdbool.h>
+
+#include <errno.h>
+#include <pthread.h>
+#include <raylib.h>
+#include <raymath.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+typedef enum {
+  eWALK,
+  eJUMP,
+  eCROUCH,
+  eDASH,
+  ePARRY,
+  eDAMAGED,
+  ePARRIED,
+  eSTUNNED,
+} StateKind;
+
+typedef struct Animation {
+  const char *path;
+  Texture2D texture;
+  Rectangle frame;
+  // animation variables;
+  size_t length;
+  size_t counter;
+  size_t speed;
+  size_t curr_frame;
+} Animation;
+
+typedef struct {
+  StateKind kind;
+  void *data;
+} State;
+
+typedef struct Player {
+  Color color;
+  uint8_t rotation;
+  Vector2 position;
+  Vector2 size;
+  State state;
+  //
+  Animation *animations;
+  size_t animations_length;
+  size_t curr_anim_index;
+} Player;
+
+bool has_state(Player *player);
+bool handle_state(Player *player);
+
+#endif // STATE_H
